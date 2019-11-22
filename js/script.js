554e7ad3b1646a436097c15641f0f9e1b33ef029
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-21 20:09:39 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
 document.getElementById("entrybutton").addEventListener("click", function()
+{
 alert("Kenton Fong: " + document.getElementById("entryinput"));
 document.getElementById("textoutput") = input;
-);
+});
 
 
 
