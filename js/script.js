c86fc2f62ae74afcb63dae754742b0178e51b0c0
8Kentonf <54601639+8Kentonf@users.noreply.github.com>
2019-11-21 20:53:56 -0600

    formatting

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-function init(){
-//add your javascrip between these two lines of code
-document.getElementById("entrybutton").addEventListener("click", myFunction);
+function init() {
+	//add your javascrip between these two lines of code
+	document.getElementById("entrybutton").addEventListener("click", myFunction);
 
-function myFunction()
-{
-  input = document.getElementById("entryinput").value;
-alert("Kenton Fong: " + input);
-document.getElementById("textoutput").innerHTML = input;
-}
-
-
-
-
+	function myFunction() {
+		input = document.getElementById("entryinput").value;
+		alert("Kenton Fong: " + input);
+		document.getElementById("textoutput").innerHTML = input;
+	}
 
 
 }
